bd583a15ede9ea2bbea3d37b760f80e015618a11
Franz <2simple233@gmail.com>
2022-09-15 03:03:43 +0800

    :bug: lab03

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -28,16 +28,17 @@ public class TimeSLList {
         AList<Double> times = new AList<>();
         AList<Integer> opCounts = new AList<>();
         int base = 1000;
-        int M = 10000;
+
         for (int i = 0; i < 8; i++) {
             if(i > 0){
                 base *= 2;
             }
-            AList<Integer> test = new AList<>();
+
+            SLList<Integer> test = new SLList<>();
             for (int j = 0; j < base; j++) {
                 test.addLast(1);
             }
-
+            int M = 10000;
             Stopwatch sw = new Stopwatch();
             for (int j = 0; j < M; j++) {
                 test.getLast();
